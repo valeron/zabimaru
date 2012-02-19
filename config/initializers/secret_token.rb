28741c5a8073a9ecde6e3bd736111b87e71bed69
Senbonzakura <senbonzakura@senbonzakura-Inspiron-N4050.(none)>
2012-02-19 21:27:52 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zabimaru::Application.config.secret_token = '69ac91a8ad6d3101a5d78e8e932250b27f2644ccf8638929f06c815b22584b840aa9ffcb89f2baed2f1cf8ba8cea26fbc73cf5031793286f1ef8a4d67a95d7a6'
